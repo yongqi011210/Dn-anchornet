812389426bbf4abdcb07f6126d613c22173aae63
yongqi kang <2826575405@qq.com>
2025-08-07 18:00:26 +0800

    v1.0

--- mmrotate/models/losses/__init__.py
+++ mmrotate/models/losses/__init__.py
@@ -1,4 +1,4 @@
-from .SimpleBCEWithLogitsLoss import AdaptiveSmoothL1Loss
+from .adaptive_smooth_l1_loss import AdaptiveSmoothL1Loss
 from .BinaryCrossEntropyLoss import BinaryCrossEntropyLoss
 __all__ = [
     'AdaptiveSmoothL1Loss','BinaryCrossEntropyLoss'
